bdac612c28d458213ed2ea0926f373726286b804
Jaman-Shah <jamanshah5400@gmail.com>
2024-05-10 14:08:57 +0600

    some design added to the footer, I will design fully later

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,15 +7,23 @@ const Footer = () => {
   return (
     <footer className="bg-gray-300 text-black p-16">
       <div className="flex justify-between items-center">
-        <div className="">
+        <div className="font-bold flex flex-col gap-4">
           <h1 className="font-bold text-3xl">Testify</h1>
-          <p>2</p>
-          <p>3</p>
+          <p>
+            <input
+              placeholder="Search here...."
+              type="text"
+              className="h-10 rounded-lg p-2"
+            />
+          </p>
+          <p>&copy; testify food company</p>
         </div>
-        <div className="border-l-2 pl-4 border-black">
-          <p>Contact :</p>
-          <p>tastify@gmail.com</p>
-          <p>Tel: +003300 </p>
+        <div className="border-l-2 pl-4 font-bold border-black">
+          <div className="flex flex-col gap-4">
+            <p className="border-b-2 border-orange-500">Contact :</p>
+            <p>tastify@gmail.com</p>
+            <p>Tel: +003300 </p>
+          </div>
         </div>
         <div className="border-l-2 pl-4 border-black">
           <div className="flex flex-col gap-4 px-4">
